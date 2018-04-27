fde8fe722439fb7758664ce12d2598e113c70b44
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-27 20:28:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -136,6 +136,7 @@ def create_task():
     		'fVigencia': fVig,
     		'CantPrecio': canpro
     	}
+	
     	tasks.append(task)
      	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     	cur = conn.cursor()
@@ -168,7 +169,7 @@ def create_task():
     	return jsonify({'task': task[0]})
 
 #######################################################################################################################################
-
+"""
 @app.route('/empresas', methods=['PUT'])   # original /<task_nom>', methods=['PUT'])
 def update_task(): #task_nom):
     if not request.json:
@@ -219,7 +220,7 @@ def delete_task(task_nom):
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
-    return jsonify({'result': True})
+    return jsonify({'result': True})"""
 
 #######################################################################################################################################
 
