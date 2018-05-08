693784b3ced72c1f9bfde070ca20980b29925eca
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-08 12:52:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,30 +164,15 @@ def create_task():
         
     	return jsonify({'task': task}), 201
     else:
-        if len(task) == 0: 
-		fVig = request.json["fVigencia"]
-    		canpro = request.json["CantPrecio"]
-                
-		task = {
-			'Empresa': nom,
-			'Sucursal': suc,
-			'fVigencia': fVig,
-			'CantPrecio': canpro
-		}
-	else:		
-		task[0]['fVigencia'] = request.json.get('fVigencia', task[0]['fVigencia'])
-    		task[0]['CantPrecio'] = request.json.get('CantPrecio', task[0]['CantPrecio'])    
-        
-    		fVig = task[0]['fVigencia']
-    		canpro = task[0]['CantPrecio']  
-		task = {
-			'Empresa': nom,
-			'Sucursal': suc,
-			'fVigencia': fVig,
-			'CantPrecio': canpro
-		}
-		
-    	
+	fVig = request.json["fVigencia"]
+    	canpro = request.json["CantPrecio"]
+	task = {
+    		'Empresa': nom,
+    		'Sucursal': suc,
+    		'fVigencia': fVig,
+    		'CantPrecio': canpro
+    	}
+    	tasks.append(task)
     	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     	cur = conn.cursor()        
     	try:
