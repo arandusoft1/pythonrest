763e82aa124c7737d30b025d75ce85de39245141
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-08 13:07:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -128,12 +128,12 @@ def create_task():
     	
     conn1 = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     con1 = conn1.cursor()
-    con1.execute("select * from Empresas where nombre ='%s' and sucursal ='%s';", (nom,suc))
+    con1.execute("select COUNT(*) from Empresas where nombre ='%s' and sucursal ='%s';", (nom,suc))
     rows = con1.fetchall()
     cont = 0
     
     for row in rows:
-    	cont = cont + 1
+    	cont = row[0]
     	
     con1.close()
     conn1.close()
@@ -164,16 +164,11 @@ def create_task():
         
     	return jsonify({'task': task}), 201
     else:
-    	fVig = request.json["fVigencia"]
-    	canpro = request.json["CantPrecio"]
+    	task[0]['fVigencia'] = request.json.get('fVigencia', task[0]['fVigencia'])
+    	task[0]['CantPrecio'] = request.json.get('CantPrecio', task[0]['CantPrecio'])    
         
-    	task = {
-    		'Empresa': nom,
-    		'Sucursal': suc,
-    		'fVigencia': fVig,
-    		'CantPrecio': canpro
-    	}
-    	#tasks.append(task)  
+    	fVig = task[0]['fVigencia']
+    	canpro = task[0]['CantPrecio']  
     	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     	cur = conn.cursor()        
     	try:
@@ -183,7 +178,7 @@ def create_task():
     		conn.rollback()
     	cur.close()
     	conn.close()
-    	return jsonify({'task': task}), 201
+    	return jsonify({'task': task[0]})
     	
 #######################################################################################################################################
 
