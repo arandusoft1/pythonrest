bb89fe5f13aef269b9c4ec52a2248b348a1ad138
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-08 12:41:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def create_task():
     nom = request.json["Empresa"]
     suc = request.json["Sucursal"]
     
-    #task = [task for task in tasks if ( task['Empresa'] == nom and task['Sucursal'] == suc )]
+    task = [task for task in tasks if ( task['Empresa'] == nom and task['Sucursal'] == suc )]
     	
     conn1 = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     con1 = conn1.cursor()
