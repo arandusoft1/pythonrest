f9d9c373f60a00aedbae60b85c6a3362e24ae0af
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-08 11:56:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -125,17 +125,17 @@ def create_task():
     suc = request.json["Sucursal"]
     
     #task = [task for task in tasks if ( task['Empresa'] == nom and task['Sucursal'] == suc )]
-    conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
-    con = conn.cursor()
-    con.execute("select * from Empresas where nombre ='%s' and sucursal ='%s';", (nom,suc))
-    rows = con.fetchall()
+    conn1 = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
+    con1 = conn1.cursor()
+    con1.execute("select * from Empresas where nombre ='%s' and sucursal ='%s';", (nom,suc))
+    rows = con1.fetchall()
     cont = 0
     
     for row in rows:
     	cont = cont + 1
     	
-    con.close()
-    conn.close()
+    con1.close()
+    conn1.close()
     
     
     if cont == 0:
