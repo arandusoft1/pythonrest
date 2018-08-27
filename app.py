94f53cd53d050a3901c03ac86c6f1ea610962be9
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-08-27 18:39:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -478,7 +478,7 @@ def create_task():
     			'fVigencia': fVig,
     			'CantPrecio': canpro
     		}
-		logging.info(nom+' '+suc+' '+fVig+' '+canpro)
+		print('kkk')
     		tasks.append(task)
     		connpost = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     		curpost = connpost.cursor()
@@ -503,7 +503,6 @@ def create_task():
     			'fVigencia': fVig,
     			'CantPrecio': canpro
     		}
-		logging.info(nom+' '+suc+' '+fVig+' '+canpro)
     		tasks.append(task) 
     		conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     		cur = conn.cursor()        
@@ -525,7 +524,6 @@ def create_task():
     		'fVigencia': fVig,
     		'CantPrecio': canpro
     	}
-	logging.info(nom+' '+suc+' '+fVig+' '+canpro)
     	#curpost.close()
     	#connpost.close()
     	return jsonify({'task': task}), 201		
