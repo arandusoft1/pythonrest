e38f98c36cca337d1d5f56253c6ce96409d0a18e
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-08-27 18:26:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -477,6 +477,7 @@ def create_task():
     			'fVigencia': fVig,
     			'CantPrecio': canpro
     		}
+		print(task)
     		tasks.append(task)
     		connpost = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     		curpost = connpost.cursor()
@@ -501,6 +502,7 @@ def create_task():
     			'fVigencia': fVig,
     			'CantPrecio': canpro
     		}
+		print(task)
     		tasks.append(task) 
     		conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     		cur = conn.cursor()        
@@ -522,6 +524,7 @@ def create_task():
     		'fVigencia': fVig,
     		'CantPrecio': canpro
     	}
+	print(task)
     	#curpost.close()
     	#connpost.close()
     	return jsonify({'task': task}), 201		
