3f94837b9149270c5018c5c2783f1c9549d83134
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-08-28 20:48:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -478,7 +478,7 @@ def create_task():
     			'fVigencia': fVig,
     			'CantPrecio': canpro
     		}
-		#print ("kkk")
+    		print ("kkk")
     		tasks.append(task)
     		connpost = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     		curpost = connpost.cursor()
